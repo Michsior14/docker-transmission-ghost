4b3ef9174a027deb26dabf8171a2b844e7f5ede2
Michał Mrozek <michalmrozek@wp.pl>
2020-07-19 22:23:00 +0200

    feat: refresh url

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -9,6 +9,9 @@ ARG_LABEL="N/A"
 CONFIG_OUTPUT="/media"
 FILEBOT_PORT=${FILEBOT_PORT:-7676}
 
+SONARR_CATEGORY=${SONARR_CATEGORY:-"sonarr"}
+RADARR_CATEGORY=${RADARR_CATEGORY:-"radarr"}
+
 FILEBOT_CMD=$(\
 echo curl \
     --data-urlencode name=\"${ARG_NAME}\" \
@@ -18,3 +21,31 @@ echo curl \
 
 echo $FILEBOT_CMD >> /config/filebot.log
 eval $FILEBOT_CMD
+
+REFRESH_NAME=""
+REFRESH_URL=""
+case $ARG_CATEGORY in
+
+    $SONARR_CATEGORY)
+        REFRESH_NAME="RescanSeries"
+        REFRESH_URL="http://sonarr:${SONARR_PORT}/api/command?apikey=${SONARR_API_KEY}"
+    ;;
+
+    $RADARR_CATEGORY)
+        REFRESH_NAME="RescanMovie"
+        REFRESH_URL="http://raddar:${RADARR_PORT}/api/command?apikey=${RADARR_API_KEY}"
+    ;;
+esac
+
+
+if [ $REFRESH_URL != "" ]; then
+    REFRESH_CMD=$(\
+        echo curl \
+            -d \"{\\\"name\\\":\\\"${REFRESH_NAME}\\\"}\" \
+            -H \"Content-Type: application/json\" \
+            -X POST \
+            ${REFRESH_URL})
+    echo $REFRESH_CMD >> /config/databases-refresh.log
+    eval $REFRESH_CMD
+fi
+
